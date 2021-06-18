aa49fd4456a80d8b717464ca320e56a15719ea22
sebastian <papafrity@gmail.com>
2021-06-17 22:42:21 -0300

    Initial commit

---
+++ app/src/main/java/kk/techbytecare/simplechatbot/startGreeting.java
@@ -0,0 +1,13 @@
+package kk.techbytecare.simplechatbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class startGreeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_greeting);
+    }
+}
